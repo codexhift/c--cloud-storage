7639e2e20e168c2fac357ad1cbde6ec6e1c8e7eb
codexhift <acchaveam@gmail.com>
2026-08-12 20:57:30 +0700

    Add trash support to Folders and FileItems with DeletedAt and IsDeleted columns

---
+++ Migrations/20260812134755_AddTrashSupport.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CloudStorage.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTrashSupport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Folders",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Folders",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "FileItems",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "FileItems",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Folders");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Folders");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "FileItems");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "FileItems");
+        }
+    }
+}
