85bb508151a3890a4da26b3cdd4978bdb7512940
codexhift <acchaveam@gmail.com>
2026-08-12 20:59:17 +0700

    Add IsDeleted and DeletedAt properties to FileItem and Folder for soft delete support

--- Models/FileItem.cs
+++ Models/FileItem.cs
@@ -21,4 +21,7 @@ public class FileItem
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    public bool IsDeleted { get; set; } = false;
+    public DateTime? DeletedAt { get; set; }
 }
--- Models/Folder.cs
+++ Models/Folder.cs
@@ -16,4 +16,7 @@ public class Folder
     public ICollection<FileItem> FileItems { get; set; } = new List<FileItem>();
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public bool IsDeleted { get; set; } = false;
+    public DateTime? DeletedAt { get; set; }
 }
